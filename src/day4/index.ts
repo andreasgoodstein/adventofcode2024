39e9452730a14c226bfbb79806e63054a1ae5a93
andreasgoodstein <24245735+andreasgoodstein@users.noreply.github.com>
2024-12-04 09:50:21 +0000

    solve day 4 part 2

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -9,6 +9,7 @@ const solve1 = (data: string[][]): number => {
   for (let y = 0; y < data.length; y++) {
     for (let x = 0; x < data[y].length; x++) {
       if (data[y][x] !== "X") {
+        // not start of xmas
         continue;
       }
 
@@ -96,7 +97,88 @@ const solve1 = (data: string[][]): number => {
   return xmasCount;
 };
 
+const solve2 = (data: string[][]): number => {
+  let masXCount = 0;
+
+  for (let y = 0; y < data.length; y++) {
+    for (let x = 0; x < data[y].length; x++) {
+      if (data[y][x] !== "A") {
+        // not center of x-mas
+        continue;
+      }
+
+      if (
+        y === 0 ||
+        x === 0 ||
+        y === data.length - 1 ||
+        x === data[y].length - 1
+      ) {
+        // on border, x-mas not possible
+        continue;
+      }
+
+      if (
+        containsMasX(
+          data[y - 1][x - 1],
+          data[y - 1][x + 1],
+          data[y + 1][x - 1],
+          data[y + 1][x + 1]
+        )
+      ) {
+        masXCount += 1;
+      }
+    }
+  }
+
+  return masXCount;
+};
+
 const doesSpellXmas = (m: string, a: string, s: string) =>
   m === "M" && a === "A" && s === "S";
 
-console.log(timeFunction(() => solve1(readData())));
+const containsMasX = (
+  upLeft: string,
+  upRight: string,
+  downLeft: string,
+  downRight: string
+): boolean => {
+  if (
+    upLeft === "M" &&
+    upRight === "M" &&
+    downLeft === "S" &&
+    downRight === "S"
+  ) {
+    return true;
+  }
+
+  if (
+    upLeft === "S" &&
+    upRight === "S" &&
+    downLeft === "M" &&
+    downRight === "M"
+  ) {
+    return true;
+  }
+
+  if (
+    upLeft === "M" &&
+    upRight === "S" &&
+    downLeft === "M" &&
+    downRight === "S"
+  ) {
+    return true;
+  }
+
+  if (
+    upLeft === "S" &&
+    upRight === "M" &&
+    downLeft === "S" &&
+    downRight === "M"
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+console.log(timeFunction(() => solve2(readData())));
