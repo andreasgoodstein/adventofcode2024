5e4973fac5853777723f4c9779be14b44cf46476
andreasgoodstein <24245735+andreasgoodstein@users.noreply.github.com>
2024-12-05 09:14:24 +0000

    solve day 5 part 1

---
+++ src/day5/index.ts
@@ -0,0 +1,48 @@
+import { readFile } from "../tools/readFile.js";
+import { timeFunction } from "../tools/timeFunction.js";
+
+type MapSet = Record<string, Set<string>>;
+
+const readData = () => readFile("day5/problem.txt").join("\n").split("\n\n");
+
+const solve1 = (rules: string[], pageList: string[]): number => {
+  const ruleMapSet = rules.reduce<MapSet>((mapSet, rule) => {
+    const [before, after] = rule.split("|");
+
+    const set = mapSet[before] ?? new Set();
+    set.add(after);
+    mapSet[before] = set;
+
+    return mapSet;
+  }, {});
+
+  const validPages: string[][] = [];
+  pageList.forEach((list) => {
+    const pages = list.split(",");
+
+    for (let i = 0; i < pages.length - 1; i++) {
+      const before = pages[i];
+      const after = pages[i + 1];
+      const ruleIsBroken = ruleMapSet[after]?.has(before) ?? false;
+      if (ruleIsBroken) {
+        return;
+      }
+    }
+
+    validPages.push(pages);
+  });
+
+  const validPagesSum = validPages.reduce<number>(
+    (sum, pages) =>
+      sum + Number.parseInt(pages[Math.floor(pages.length / 2)], 10),
+    0
+  );
+
+  return validPagesSum;
+};
+
+const solve2 = () => {};
+
+const [rules, pages] = readData().map((section) => section.split("\n"));
+
+console.log(timeFunction(() => solve1(rules, pages)));
